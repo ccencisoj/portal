5b32cf548e1b5679ebaa1cbfa97a8361075b7e75
cristian <bizapro123@gmail.com>
2022-06-11 20:04:29 -0500

    v6

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,12 @@
+import { terser } from "rollup-plugin-terser";
+
 export default {
   input: "src/index.js",
   output: {
     file: "dist/index.js",
     format: "cjs"
-  }
+  },
+  plugins: [
+    terser()
+  ]
 };
